c45aac2335ec9101a812d22d7e74c5d4017972a7
raulrfc <raul.franco.ce@gmail.com>
2014-09-29 18:32:30 +0200

    Ejercicio2
    Working version

--- Ejercicio2/src/Ejercicio2.cpp
+++ Ejercicio2/src/Ejercicio2.cpp
@@ -1,9 +1,9 @@
 //============================================================================
 // Name        : Ejercicio2.cpp
 // Author      : Raúl Franco
-// Version     : 1.0
+// Version     : 1.2
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Carga los datos de un archivo y los ordena de mayor a menor
 //============================================================================
 
 #include <iostream>
@@ -27,24 +27,32 @@ int main() {
 	int registro;
 	mialumno alumno;
 
-	map<float, int, less<float> > m;
-	map<float, int, less<float> >::iterator inicio;
+	//Se crea un multimapa para poder almacenar las notas medias ordenadas
+	multimap<float, int, less<float> > m;
+	//Iterador inverso para poder mostrar los resultados del mapa ordenados de mayor a menor
+	multimap<float, int, less<float> >::reverse_iterator inicio;
 
+
+	//Apertura y lectura del archivo
 	ifstream fentrada("c:\\datos\\alumnos.dat", ios::in | ios::binary);
+	fentrada.read(reinterpret_cast<char*>(&alumno), sizeof(mialumno));
 
-	for (int i = 0; i < 100; i++) {
+	//Se posiciona con cada iteración, lee y guarda los datos en el mapa
+	for (int i = 0; i < 100; i++){
+		fentrada.seekg(i*sizeof(mialumno), ios::beg);
 		fentrada.read(reinterpret_cast<char*>(&alumno), sizeof(mialumno));
 		media = (alumno.nota1 + alumno.nota2 + alumno.nota3) / 3;
 		registro = alumno.clase;
 		m.insert(pair<float, int>(media, registro));
 	}
 
-	for (inicio = m.begin(); inicio != m.end(); inicio++) {
+	//Bucle para mostrar los datos del mapa de mayor a menor
+	for (inicio = m.rbegin(); inicio != m.rend(); inicio++) {
 		fentrada.seekg(((*inicio).second-1)*sizeof(mialumno), ios::beg);
 		fentrada.read(reinterpret_cast<char*>(&alumno), sizeof(mialumno));
 		cout << alumno.clase << " " << alumno.apellido << " " << alumno.nota1 << " " << alumno.nota2 << " " << alumno.nota3 << " Media: " << (*inicio).first << endl;
-
 	}
+
 	fentrada.close();
 
 	return 0;
